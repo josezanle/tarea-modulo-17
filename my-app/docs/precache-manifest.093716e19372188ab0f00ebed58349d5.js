2e8c933cbb7e828f56f077f1bbdb78f135c7217e
josezanle <63666176+josezanle@users.noreply.github.com>
2020-09-21 09:17:11 -0300

    tarea modulo 17 JournalApp Styles with Sass

---
+++ my-app/docs/precache-manifest.093716e19372188ab0f00ebed58349d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feec042608199946233c98c39be63eb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63e3fb4f170f1ea8744a",
+    "url": "/static/css/main.ecbe42f0.chunk.css"
+  },
+  {
+    "revision": "42061cae751ab21674ff",
+    "url": "/static/js/2.abba4c3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.abba4c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e3fb4f170f1ea8744a",
+    "url": "/static/js/main.06dc8bd1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
